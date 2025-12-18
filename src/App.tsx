61f80362ccdb8ebb9a7d6f92c83042afbc84146f
xin <a94011500@gmail.com>
2025-12-18 15:29:23 +0800

    15

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 // import { useState } from 'react'
+import { useState } from 'react';
 import './App.css'
 import Layout from './components/Layout'
 const baseClasses =
@@ -10,7 +11,16 @@ const baseClasses =
 // const defaultClasses = 
 //             "bg-white text-gray-800 border border-gray-200 hover:bg-gray-50 active:bg-gray-100";            
 
+
+
+
 function App() {
+  const [input1, setInput1] = useState("");
+  const [op, setOp] = useState("");
+  const [eq, setEq] = useState(false);
+  const [input2, setInput2] = useState("");
+  const [ans, setAns] = useState(0);
+  
 
   const signList = [
     '%', 'CE', 'C', '⌫', // ⌫ 代表退格/刪除鍵 (Backspace)
@@ -21,18 +31,52 @@ function App() {
     '±', '0', '.', '=',
   ];
 
- // const [count, setCount] = useState(0)
+  const isNum = (sign: string) => {
+    return !isNaN(parseFloat(sign))
+  };
+
+  const handleCalc = (sign: string)=> {
+    if (isNum(sign)) {
+      if(op) {
+        setInput2(input2 + sign);
+      } else {
+        setInput1(input1 + sign);
+      }
+      return;
+    }
+    if("+" === sign){
+      setOp(sign);
+      return;
+    }
+
+    if("=" === sign){
+      if("+" === op){
+        setAns(parseFloat(input1) +parseFloat(input2) );
+        setEq(true);
+      }
+     }
+  };
+
+  const getDisplayText = () => {
+    if(eq){
+      return ans+"";
+    }
+    if(op){
+      return input2;
+    }
+    return input1;
+  };
 
   return (
     <Layout>
-      <input type='text' className="bg-green-100 p-4 mb-4 rounded-lg shadow-inner" />
+      <input type='text' value={getDisplayText()} className="bg-green-100 p-4 mb-4 rounded-lg shadow-inner" />
       <div className="grid grid-cols-4 gap-2 bg-gray-100 p-4 rounded-xl shadow-2xl max-w-sm mx-auto">
         {
           signList.map(
             (sign, index) => {
 
               return (
-                <div key={index} className={baseClasses}>
+                <div key={index} className={baseClasses} onClick={()=>{  handleCalc(sign); }} >
                   {sign}
                 </div>
               )
